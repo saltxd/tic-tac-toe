72dc8c920fbcf5a1b54f65ec3b8085bcaf6534ae
saltxd <74526520+saltxd@users.noreply.github.com>
2023-04-28 10:27:45 -0600

    Add check winning function to gameboard

--- script.js
+++ script.js
@@ -21,6 +21,40 @@ const Gameboard = (() => {
         ];
     }
 
+    const checkForWinner = () => {
+        //check rows
+        for (let i = 0; i < 3; i++) {
+          if (board[i][0] !== '' && board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
+              return board[i][0];
+          }
+        }
+        
+        //check columns
+        for (let i = 0; i < 3; i++) {
+          if (board[0][i] !== '' && board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
+            return board[0][i];
+          }
+        }
+
+        // Check diagonals
+        if (board[0][0] !== '' && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
+          return board[0][0];
+        }
+
+        if (board[0][2] !== '' && board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
+            return board[0][2];
+        }
+        
+        // Check for tie
+        if (board.every(row => row.every(cell => cell !== ''))) {
+          return 'tie';
+        }
+
+         // Game is not over yet
+        return null;
+        };
+
+
     return {
         getBoard,
         placeSymbol,
