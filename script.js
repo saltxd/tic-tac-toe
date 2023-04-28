8d23e9f94b102c7a5c108c9c5ee287d8a4f71ceb
saltxd <74526520+saltxd@users.noreply.github.com>
2023-04-28 08:02:35 -0600

    Create player object

--- script.js
+++ script.js
@@ -27,3 +27,10 @@ const Gameboard = (() => {
         clearBoard
     };
 })();
+
+const createPlayer = (name, symbol) => {
+    return {name, symbol};
+}
+
+const player1 = createPlayer('Player 1', 'X');
+const player2 = createPlayer('Player 2', 'O');
